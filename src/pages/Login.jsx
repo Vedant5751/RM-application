eac7dc56f6dc7d875876a017602a5d3a778d41c6
Vedant Patil <vedant1346@gmail.com>
2024-05-31 00:46:45 +0530

    Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,67 @@ import React from 'react'
 export default function Login() {
   return (
     <div>
-      <h1>login</h1>
+      <div className="flex h-screen">
+        <div className="items-center justify-center w-1/2 bg-white ">
+          <img src="./LoginLogo.svg" alt="Login Logo" />
+        </div>
+        <div className="w-full bg-gray-100 lg:w-1/2 flex items-center justify-center">
+          <div className="max-w-md w-full p-6">
+            <h1 className="text-5xl font-semibold mb-6 text-black text-center">
+              Log In
+            </h1>
+            <form className="space-y-4">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  className="mt-1 p-2 w-full border rounded-lg focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="mt-1 p-2 w-full border rounded-lg focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                />
+              </div>
+              <div className="text-right hover:underline">
+                <a href="#" className="text-indigo-700">
+                  Forgot Password?
+                </a>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full bg-black text-white mt-4 p-2 rounded-lg hover:bg-gray-800 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
+                >
+                  Login
+                </button>
+              </div>
+              <div className="text-center">
+                <a href="#" className="text-sm hover:underline">
+                  Unable to Connect? Get in touch with our
+                  <span className="font-bold"> Support Team</span>
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
