c9a93a448c67161d172713c1d214510ba3831a71
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-05-31 00:45:05 +0530

    sidebar finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaTachometerAlt, FaBriefcase, FaFileInvoiceDollar, FaProjectDiagram, FaUsers, FaBars, FaTimes } from 'react-icons/fa';
+import { FaTachometerAlt, FaBriefcase, FaFileInvoiceDollar, FaProjectDiagram, FaUsers, FaBars, FaTimes, FaComment, FaSignOutAlt } from 'react-icons/fa';
 
 const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(true);
@@ -45,6 +45,22 @@ const Sidebar = () => {
                         <a href="#employee" className="flex items-center p-2 space-x-3 rounded-md hover:bg-gray-700">
                             <FaUsers size={24} />
                             {isOpen && <span>Employee</span>}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="mt-auto text-lg">
+                <ul className="space-y-6 text-gray-100">
+                    <li className="rounded-sm">
+                        <a href="#feedback" className="flex items-center p-2 space-x-3 rounded-md hover:bg-gray-700">
+                            <FaComment size={24} />
+                            {isOpen && <span>Feedback</span>}
+                        </a>
+                    </li>
+                    <li className="rounded-sm">
+                        <a href="#logout" className="flex items-center p-2 space-x-3 rounded-md hover:bg-gray-700">
+                            <FaSignOutAlt size={24} />
+                            {isOpen && <span>Logout</span>}
                         </a>
                     </li>
                 </ul>
