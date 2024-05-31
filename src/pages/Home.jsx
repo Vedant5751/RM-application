0afb46a93e7ed32f3bc2ef68f1a5cc7d4621f78e
Vedant Patil <vedant1346@gmail.com>
2024-05-31 13:15:58 +0530

    template

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import Sidebar from '../components/Sidebar'
 export default function Home() {
   return (
     <>
-
       <div className="flex">
             <Sidebar />
             <div className="flex-1 p-10">
