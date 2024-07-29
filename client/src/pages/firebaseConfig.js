7c95f743136e0ee2f89d769152f3c45a0a4ee799
Ritinikhil <124866156+Ritinikhil@users.noreply.github.com>
2024-07-30 00:28:35 +0530

    Update firebaseConfig.js

--- client/src/pages/firebaseConfig.js
+++ client/src/pages/firebaseConfig.js
@@ -6,13 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBzXD97KZtN5eebDbam0BVfW9V1oKvayRI",
-    authDomain: "resourcify-4f66b.firebaseapp.com",
-    projectId: "resourcify-4f66b",
-    storageBucket: "resourcify-4f66b.appspot.com",
-    messagingSenderId: "323586964469",
-    appId: "1:323586964469:web:0571363c7420faa6857ebc",
-    measurementId: "G-K3R4N83SM8"
+
 };
 
 // Initialize Firebase
